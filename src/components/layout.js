ecd7c665d7b3c769d2efc48f2dfd8235e2bfc647
domobritton <domonicbritton@gmail.com>
2018-11-12 15:57:19 -0800

    Add sass plugin

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,7 +4,7 @@ import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
-import './layout.css'
+import './layout.scss'
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -28,7 +28,6 @@ const Layout = ({ children }) => (
         >
           <html lang="en" />
         </Helmet>
-        <Header siteTitle={data.site.siteMetadata.title} />
         <div
           style={{
             margin: '0 auto',
